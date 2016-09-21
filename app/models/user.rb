5fef75880e454246596f227d326e0b2784f29edb
pancho <plabradorjr@gmail.com>
2016-09-20 20:06:57 -0500

    implemented devise for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :password, :presence => true,
+  					   :confirmation => true,
+  					   :length => {:within => 6..40},
+                       :on => :create,
+                       :if => :password,
+                       :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\z/}
 end
